7740b407052c71a19e2e178b263d3725194f3b44
Srinjoy Chakravarty <chaxz93@gmail.com>
2012-10-10 17:27:05 +0300

    paws

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,15 @@
 PetApp::Application.routes.draw do
-
-
-
   resources :animals
-
   resources :pets
-
-
-
   resources :owners
+
   root :to => 'pages#home'
+
+  match '/home', :to => 'pages#home'
   match '/about', :to => 'pages#about'
   match '/contact', :to => 'pages#contact'
   match '/privacy', :to => 'pages#privacy'
   match '/signup', :to => 'owners#new'
-
   match '/owners', :to => 'owners#index'
 
 
